bec5845324653ce22fbbe0c827c48802740cedfc
zc00l <andre.marques@fatec.spgov.br>
2017-05-03 13:50:02 -0300

    First commit

---
+++ ghubs.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+# ====================================================================
+# GitHub Sync (ghubs) is a perl script to sync with one user profile
+# from github.com using GitHub API.
+# ====================================================================
+
+use strict; use warnings;
+
+my $config_file = "ghubs.conf";
+
+
+sub read_data()
+{
+  open(my $fp, "<", "");
+  print $fp . "\n";
+}
+
+sub main()
+{
+  read_data();
+  return 0;
+}
+
+main();
